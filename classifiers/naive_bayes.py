dc9c7b94a8a499d08850cadd3e075df59d402a8a
tonyin <tonyyyin@gmail.com>
2014-10-19 23:03:22 -0700

    naive bayes implementation

--- classifiers/naive_bayes.py
+++ classifiers/naive_bayes.py
@@ -1,25 +1,52 @@
 #!/usr/bin/env python
 
-import pandas
-import zipfile
+import pandas as pd
+import numpy as np
+import string
+
+PROB = 0.5 # Probability threshold for accepting a keyword as a tag, given it exists in the title
+
 
 def nb_train(train):
 
-	counts = {}
+    # Create dictionary of all possible tags
+    kws = np.hstack(train['Tags'])
+    kws = np.unique(kws)
+    counts = {}
+    for kw in kws:
+        counts.setdefault(kw, {})
+        counts[kw].setdefault('tag', 0)
+        counts[kw].setdefault('title', 0)
+        counts[kw].setdefault('both', 0)
 
-	return counts
+    # Find frequency of tag kw appearances
+    for title_kws, tag_kws in zip(train['Title'], train['Tags']):
+        for kw in set(title_kws).difference(tag_kws):
+            if kw in counts:
+                counts[kw]['title'] += 1
+        for kw in set(title_kws).intersection(tag_kws):
+            counts[kw]['both'] += 1
+        for kw in set(tag_kws).difference(title_kws):
+            counts[kw]['tag'] += 1
+    
+    # Make list of keywords that pass the PROB threshold for keyword acceptance
+    nb_tags = []
+    for kw in counts:
+        title_only = counts[kw]['title']
+        both= counts[kw]['both']
+        if both + title_only == 0: continue
+        prob_tagged = 1.0 * both / (both + title_only)
+        if prob_tagged > PROB:
+            nb_tags.append(kw)
+
+    return nb_tags
+
 
 def naive_bayes(train, pred):
     
-	counts = nb_train(train)
+    nb_tags = nb_train(train)
+    nb_filter = lambda x: list(set(x).intersection(nb_tags))
+    str_join = lambda x: ' '.join(x)
+    pred['Tags'] = pd.Series(pred['Title'].map(nb_filter).map(str_join))
 
     return pred
-
-# import string
-# puncs = list(string.punctuation)
-# for s in train['Tags'].values:
-#     for kw in s
-#         for c in kw:
-#             if c in puncs:
-#                 puncs.remove(c)
-# puncs = ''.join(puncs)
--- classify.py
+++ classify.py
@@ -2,29 +2,45 @@
 
 import argparse
 import time
-import pandas
-import zipfile
+import pandas as pd
+import zipfile as zf
+import string
 
 from classifiers import naive_bayes
 
 def parse():
     parser = argparse.ArgumentParser(description='Generate predictions from train data and output results to csv.')
-    parser.add_argument('-t', metavar='filename', default='data/Train.zip', help='specify Train zip file (data/[filename])')
-    parser.add_argument('-p', metavar='filename', default='data/Pred.csv', help='specify Pred csv file (data/[filename]')
+    parser.add_argument('-t', metavar='path_to_file', default='data/Train.zip', help='specify Train zip file (data/[filename])')
+    parser.add_argument('-p', metavar='path_to_file', default='data/Pred.csv', help='specify Pred csv file (data/[filename])')
     return parser.parse_args()
 
 def main():
     args = parse()
 
-    if zipfile.is_zipfile(args.t):
-        with zipfile.ZipFile(args.t, 'r') as zf:
-            f = args.t.split('/')[1].split('.')[0]
-            train = pandas.read_csv(zf.open(f + '.csv'), usecols=['Id', 'Title', 'Tags'])
+    # Load training data
+    if zf.is_zipfile(args.t):
+        with zf.ZipFile(args.t, 'r') as f:
+            name = args.t.split('/')[1].split('.')[0]
+            train = pd.read_csv(f.open(name + '.csv'), usecols=['Id', 'Title', 'Tags'])
 
-    pred = train[['Id', 'Title']]
+    # Clean tags
+    train['Tags'] = train['Tags'].str.lower().str.split(' ')
+
+    # Get list of unused punctuation and clean titles
+    puncs = list(string.punctuation)
+    for s in train['Tags'].values:
+        for kw in s:
+            for c in kw:
+                if c in puncs:
+                    puncs.remove(c)
+    puncs = ''.join(puncs)
+    train['Title'] = train['Title'].str.lower().str.replace('[' + puncs + ']', '').str.split(' ')
     
+    # Apply classifier(s)
+    pred = train[['Id', 'Title']]
     pred = naive_bayes(train, pred)
-    pred.to_csv(args.p)
+    
+    pred.to_csv(args.p, columns=['Id', 'Tags'], index=False)
 
 if __name__ == '__main__':
     start = time.time()
